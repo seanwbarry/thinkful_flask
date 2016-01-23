f9579cd00d62424063b3e589f6cb31afc5e9f9b0
seanwbarry <sean.w.barry@gmail.com>
2016-01-23 06:06:43 +0000

    some jinja2 stuff

--- hello_world.py
+++ hello_world.py
@@ -1,50 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
 from os import environ
 
 app = Flask(__name__)
 #tell the Flask application where it is being run from
-#@app.route("/")
+@app.route("/")
+def template_teset():
+    return render_template('base.html',name="asdf")
+
+
 @app.route("/hello")
 def say_hi():
-    return "Hello World!"
+    return render_template('basic.html', name='world')
 
-#only one of the below functions will run - why is that???
-
-"""
-@app.route("/hello/<name>")    
-def hi_person(name):
-    return "Hello {}!".format(name.title())
-"""
 @app.route("/hello/<name>")
 def hello_person(name):
-    html = """
-        <h1>
-            Hello {}!
-        </h1>
-        <p>
-            Here's a picture of a kitten. Awww...
-        </p>
-        <img src="http://placekitten.com/g/200/300">
-    """
-    print(__name__.split(".")[0])
-    print('next')
-    #print(__name__.split(".")[1])
-    #print('next')
-    print(__name__.split(".")[-1])
-    print('next')
-    return html.format(name.title())
-    
+    print(name)
+    print('tried to print name')
+    return render_template('basic.html', greeting=name)
+
 @app.route("/jedi/<first>/<second>")
 def jedi_name(first, second):
     part1 = second[:3]
     part2 = first[:2]
-    html = """
-        <h1>
-            Hello {}{}!
-        </h1>
-    """
-    return html.format(part1,part2)
-    
+    return render_template('basic.html', greeting=(part1+part2))
+
+
 if __name__ == "__main__":
-    app.run(host=environ['IP'],
-        port=int(environ['PORT']))
+    app.run(debug=True, host="0.0.0.0", port=8080)
